ccabc3ab1092f60d26ba2e9ac62e5f7c0a5d3dd4
kimberly blanco <michel_kimberly@hotmail.com>
2017-07-03 10:06:44 -0500

    se creo clase

---
+++ RA-KimberlyMichelEstradaBlanco/BussinesEntities/EstudiosRF01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesEntities
+{
+    public class EstudiosRF01
+    {
+
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+        private string Categoría { get; set; }
+        private double Costo { get; set; }
+        private bool Estado { get; set; }
+
+
+        public EstudiosRF01(string nombre,string descripcion,string categoria,double costo,bool estado)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Categoría = categoria;
+            Costo = costo;
+            Estado = estado;
+        }
+
+       
+
+
+    }
+
+
+
+
+}
