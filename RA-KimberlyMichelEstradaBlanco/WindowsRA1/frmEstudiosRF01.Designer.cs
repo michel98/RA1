40da57472c724937d196f64f32b7882a5fa6d058
kimberly blanco <michel_kimberly@hotmail.com>
2017-07-03 11:23:07 -0500

    Se elimino el form1 que viene por defecto y se agregaron combobox y botones a la ventana de gestion de estudios

---
+++ RA-KimberlyMichelEstradaBlanco/WindowsRA1/frmEstudiosRF01.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsRA1
+{
+    partial class frmEstudiosRF01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.cboEstudios = new System.Windows.Forms.ComboBox();
+            this.lblEstudio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(76, 138);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(132, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar nuevo estudio";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(296, 138);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(132, 23);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cboEstudios
+            // 
+            this.cboEstudios.FormattingEnabled = true;
+            this.cboEstudios.Location = new System.Drawing.Point(76, 51);
+            this.cboEstudios.Name = "cboEstudios";
+            this.cboEstudios.Size = new System.Drawing.Size(352, 21);
+            this.cboEstudios.TabIndex = 2;
+            // 
+            // lblEstudio
+            // 
+            this.lblEstudio.AutoSize = true;
+            this.lblEstudio.Location = new System.Drawing.Point(73, 35);
+            this.lblEstudio.Name = "lblEstudio";
+            this.lblEstudio.Size = new System.Drawing.Size(45, 13);
+            this.lblEstudio.TabIndex = 3;
+            this.lblEstudio.Text = "Estudio:";
+            // 
+            // frmEstudiosRF01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 200);
+            this.Controls.Add(this.lblEstudio);
+            this.Controls.Add(this.cboEstudios);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "frmEstudiosRF01";
+            this.Text = "Gestión de estudios";
+            this.Load += new System.EventHandler(this.frmEstudiosRF01_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.ComboBox cboEstudios;
+        private System.Windows.Forms.Label lblEstudio;
+    }
+}
---
+++ RA-KimberlyMichelEstradaBlanco/WindowsRA1/frmEstudiosRF01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsRA1
+{
+    public partial class frmEstudiosRF01 : Form
+    {
+        public frmEstudiosRF01()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEstudiosRF01_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
