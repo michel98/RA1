eed7b86047a99006b5696b4670b893d903e18339
kimberly blanco <michel_kimberly@hotmail.com>
2017-07-10 19:48:33 -0500

    se crearon colores

--- RA-KimberlyMichelEstradaBlanco/WindowsRA1/Recepcion/caja de cobro/frmImprimirPago.Designer.cs
+++ RA-KimberlyMichelEstradaBlanco/WindowsRA1/Recepcion/caja de cobro/frmImprimirPago.Designer.cs
@@ -74,11 +74,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(293, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(190, 22);
+            this.label3.Size = new System.Drawing.Size(194, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Comprobante de Pago";
             // 
@@ -202,7 +202,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Green;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
             this.ClientSize = new System.Drawing.Size(773, 384);
             this.Controls.Add(this.btnImprimir);
             this.Controls.Add(this.label8);
